a4439e114c8ca4fca013cce2160d163888576a5c
RJHarrisUK <rjharrishome@yahoo.co.uk>
2019-06-12 12:05:28 +0100

    added CR-D JS

--- resources/scripts/getaccountbyid.js
+++ resources/scripts/getaccountbyid.js
@@ -1,16 +1,19 @@
+
+// READ - Get specific classroom (working - returns object)
 function getClassroomById() {
     let req = new XMLHttpRequest();
 
     req.onload = function () {
-        req.responseText;
-
+        let response = JSON.parse(req.responseText);
+        console.log(response);
     }
     let id = Number(document.getElementById("id").value);
     req.open("GET", "http://localhost:8080/APIDevAssessment/api/classroom/findClassroom/" + id);
     req.send();
+
     console.log(req);
 }
-
+// READ - Get all classrooms (working - returns object)
 function getAllClassrooms() {
     let req = new XMLHttpRequest();
 
@@ -20,22 +23,46 @@ function getAllClassrooms() {
             console.log(i);
         }
     }
-
     req.open("GET", "http://localhost:8080/APIDevAssessment/api/classroom/getAllClassrooms");
-    req.send("Get all classrooms");
-    console.log(req)
+    req.send("Get All Classrooms");
+    console.log(req);
 }
-
+// CREATE - Create a classroom (working - returns message)
 function createClassroom() {
+    let trainer = { trainer: document.getElementById("trainer").value }
+    let req = new XMLHttpRequest();
+
+    req.onload = function () {
+        console.log(req.responseText);
+    }
+    req.open("POST", "http://localhost:8080/APIDevAssessment/api/classroom/createClassroom");
+    req.send(JSON.stringify(trainer));
+}
+// UPDATE - Update a classroom (not built yet)
+function updateClassroom() {
     let req = new XMLHttpRequest();
 
     req.onload = function () {
         let response = JSON.parse(req.responseText);
+        console.log(response);
     }
+    let id = Number(document.getElementById("id").value);
+    req.open("PUT", "http://localhost:8080/APIDevAssessment/api/classroom/updateClassroom/" + id);
+    req.send();
 
-    req.open("POST", "http://localhost:8080/APIDevAssessment/api/classroom/createClassroom");
-    req.send("Get all classrooms");
-    console.log(req)
+    console.log(req);
 }
+// DELETE - Delete a classroom (working - returns message)
+function deleteClassroom() {
+    let req = new XMLHttpRequest();
 
+    req.onload = function () {
+        let response = JSON.parse(req.responseText);
+        console.log(response);
+    }
+    let id = Number(document.getElementById("id").value);
+    req.open("DELETE", "http://localhost:8080/APIDevAssessment/api/classroom/deleteClassroom/" + id);
+    req.send();
 
+    console.log(req);
+}
